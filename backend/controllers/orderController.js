cac2e6c0492fdbedb60c3a05d41acbf51db3ed5e
Harshit <18harshit052005@gmail.com>
2026-09-21 12:22:52 +0530

    Fix create order request body

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,9 +7,10 @@ const razorpayInstance = new razorpay({
     key_id: process.env.RAZORPAY_KEY_ID,
     key_secret: process.env.RAZORPAY_SECRET,
 })
+export const createOrder = async (req, res) => {
+  
 const { courseId, userId } = req.body;
 
-export const createOrder = async (req, res) => {
   try {
     const { courseId } = req.body;
 
